c824d27c1a2b72bfc3bc4937a70907acade4fd2a
arjunn2313 <arjunnks123@gmail.com>
2026-02-21 12:45:02 +0530

    ramdan event

--- src/components/christmas/SantaWaving.jsx
+++ src/components/christmas/SantaWaving.jsx
@@ -1,7 +1,7 @@
 const SantaWaving = () => {
   return (
     <img
-      src="ramdan.gif" // Update path based on your folder
+      src="ramdan.gif" 
       alt="Santa Waving"
       className="santa-waving"
     />
